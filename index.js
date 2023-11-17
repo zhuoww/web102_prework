d80daf2ad3656f5264f5f3a7e6c9cfa2705521e0
zhuoww <zhuoww1008@gmail.com>
2023-11-17 00:35:32 -0800

    Adds Challenge5

--- index.js
+++ index.js
@@ -97,8 +97,12 @@ function filterUnfundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have not yet met their goal
+  let listOfNotMetGoal = GAMES_JSON.filter((game) => {
+    return game.pledged < game.goal;
+  });
 
   // use the function we previously created to add the unfunded games to the DOM
+  addGamesToPage(listOfNotMetGoal);
 }
 
 // show only games that are fully funded
@@ -106,8 +110,11 @@ function filterFundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have met or exceeded their goal
-
+  let listOfMetGoal = GAMES_JSON.filter((game) => {
+    return game.pledged >= game.goal;
+  });
   // use the function we previously created to add unfunded games to the DOM
+  addGamesToPage(listOfMetGoal);
 }
 
 // show all games
@@ -115,6 +122,7 @@ function showAllGames() {
   deleteChildElements(gamesContainer);
 
   // add all games from the JSON data to the DOM
+  addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -123,6 +131,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
