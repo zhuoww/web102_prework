95a442e7fd0deb656c92a985e3f74e3c58aa38f4
zhuoww <zhuoww1008@gmail.com>
2023-11-17 00:08:03 -0800

    Adds Challenge4

--- index.js
+++ index.js
@@ -66,16 +66,25 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
+const totalContributions = GAMES_JSON.reduce((acc, game) => {
+  return acc + game.backers;
+}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = `${totalContributions.toLocaleString("en-US")}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
 // set inner HTML using template literal
+const totalMoneyPledged = GAMES_JSON.reduce((acc, game) => {
+  return acc + game.pledged;
+}, 0);
+raisedCard.innerHTML = `$${totalMoneyPledged.toLocaleString("en-US")}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+gamesCard.innerHTML = `${GAMES_JSON.length}`;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
